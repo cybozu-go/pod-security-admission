aba8ec813d731a6396fda038c28ee77b3c96e9eb
Masayuki Ishii <masa213f@gmail.com>
2022-07-22 02:39:55 +0000

    Update ginkgo to v2
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- hooks/suite_test.go
+++ hooks/suite_test.go
@@ -13,7 +13,7 @@ import (
 
 	//+kubebuilder:scaffold:imports
 	"github.com/cybozu-go/pod-security-admission/hooks/validators"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	admissionv1 "k8s.io/api/admission/v1"
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
@@ -24,7 +24,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -52,9 +51,7 @@ var testCancel context.CancelFunc
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"Webhook Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	RunSpecs(t, "Webhook Suite")
 }
 
 var _ = BeforeSuite(func() {
@@ -200,7 +197,7 @@ var _ = BeforeSuite(func() {
 		err = k8sClient.Create(testCtx, ns)
 		Expect(err).NotTo(HaveOccurred())
 	}
-}, 60)
+})
 
 var _ = AfterSuite(func() {
 	testCancel()
