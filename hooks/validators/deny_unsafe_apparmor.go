1a1ee7b3d03385dcc2002b890c1c4a7d47a728ee
zeroalphat <taichi-takemura@cybozu.co.jp>
2026-02-17 16:09:11 +0900

    Add AppArmor validation test cases and refactor validation logic
    Signed-off-by: zeroalphat <taichi-takemura@cybozu.co.jp>

--- hooks/validators/deny_unsafe_apparmor.go
+++ hooks/validators/deny_unsafe_apparmor.go
@@ -24,53 +24,40 @@ func (v DenyUnsafeAppArmor) Validate(ctx context.Context, pod *corev1.Pod) field
 	}
 
 	p = field.NewPath("spec").Child("SecurityContext")
-	hasPodAppArmorProfile := pod.Spec.SecurityContext != nil && pod.Spec.SecurityContext.AppArmorProfile != nil
-	if hasPodAppArmorProfile {
-		isTypeRuntimeDefault := pod.Spec.SecurityContext.AppArmorProfile.Type == corev1.AppArmorProfileTypeRuntimeDefault
-		isTypeLocalhost := pod.Spec.SecurityContext.AppArmorProfile.Type == corev1.AppArmorProfileTypeLocalhost
-		hasNotAllowedType := !(isTypeRuntimeDefault || isTypeLocalhost)
-		if hasNotAllowedType {
-			errs = append(errs, field.Forbidden(p, fmt.Sprintf("%v is not an allowed AppArmor profile", pod.Spec.SecurityContext.AppArmorProfile.Type)))
-		}
+	if pod.Spec.SecurityContext != nil && pod.Spec.SecurityContext.AppArmorProfile != nil {
+		errs = append(errs, validateAppArmorProfile(p, pod.Spec.SecurityContext.AppArmorProfile)...)
 	}
 
 	p = p.Child("containers")
 	for i, co := range pod.Spec.Containers {
-		hasPodAppArmorProfile := co.SecurityContext != nil && co.SecurityContext.AppArmorProfile != nil
-		if hasPodAppArmorProfile {
-			isTypeRuntimeDefault := co.SecurityContext.AppArmorProfile.Type == corev1.AppArmorProfileTypeRuntimeDefault
-			isTypeLocalhost := co.SecurityContext.AppArmorProfile.Type == corev1.AppArmorProfileTypeLocalhost
-			hasNotAllowedType := !(isTypeRuntimeDefault || isTypeLocalhost)
-			if hasNotAllowedType {
-				errs = append(errs, field.Forbidden(p.Index(i), fmt.Sprintf("%v is not an allowed AppArmor profile", co.SecurityContext.AppArmorProfile.Type)))
-			}
+		if co.SecurityContext != nil && co.SecurityContext.AppArmorProfile != nil {
+			errs = append(errs, validateAppArmorProfile(p.Index(i), co.SecurityContext.AppArmorProfile)...)
 		}
 	}
 
 	p = p.Child("initContainers")
 	for i, co := range pod.Spec.InitContainers {
-		hasPodAppArmorProfile := co.SecurityContext != nil && co.SecurityContext.AppArmorProfile != nil
-		if hasPodAppArmorProfile {
-			isTypeRuntimeDefault := co.SecurityContext.AppArmorProfile.Type == corev1.AppArmorProfileTypeRuntimeDefault
-			isTypeLocalhost := co.SecurityContext.AppArmorProfile.Type == corev1.AppArmorProfileTypeLocalhost
-			hasNotAllowedType := !(isTypeRuntimeDefault || isTypeLocalhost)
-			if hasNotAllowedType {
-				errs = append(errs, field.Forbidden(p.Index(i), fmt.Sprintf("%v is not an allowed AppArmor profile", co.SecurityContext.AppArmorProfile.Type)))
-			}
+		if co.SecurityContext != nil && co.SecurityContext.AppArmorProfile != nil {
+			errs = append(errs, validateAppArmorProfile(p.Index(i), co.SecurityContext.AppArmorProfile)...)
 		}
 	}
 
 	p = p.Child("ephemeralContainers")
 	for i, co := range pod.Spec.EphemeralContainers {
-		hasPodAppArmorProfile := co.SecurityContext != nil && co.SecurityContext.AppArmorProfile != nil
-		if hasPodAppArmorProfile {
-			isTypeRuntimeDefault := co.SecurityContext.AppArmorProfile.Type == corev1.AppArmorProfileTypeRuntimeDefault
-			isTypeLocalhost := co.SecurityContext.AppArmorProfile.Type == corev1.AppArmorProfileTypeLocalhost
-			hasNotAllowedType := !(isTypeRuntimeDefault || isTypeLocalhost)
-			if hasNotAllowedType {
-				errs = append(errs, field.Forbidden(p.Index(i), fmt.Sprintf("%v is not an allowed AppArmor profile", co.SecurityContext.AppArmorProfile.Type)))
-			}
+		if co.SecurityContext != nil && co.SecurityContext.AppArmorProfile != nil {
+			errs = append(errs, validateAppArmorProfile(p.Index(i), co.SecurityContext.AppArmorProfile)...)
 		}
 	}
 	return errs
+}
+
+func validateAppArmorProfile(p *field.Path, profile *corev1.AppArmorProfile) field.ErrorList {
+	if profile.Type == corev1.AppArmorProfileTypeRuntimeDefault {
+		return nil
+	}
+	name := string(profile.Type)
+	if profile.Type == corev1.AppArmorProfileTypeLocalhost && profile.LocalhostProfile != nil {
+		name = fmt.Sprintf("%s/%s", profile.Type, *profile.LocalhostProfile)
+	}
+	return field.ErrorList{field.Forbidden(p, fmt.Sprintf("%s is not an allowed AppArmor profile", name))}
 }
